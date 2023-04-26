2fe4bff296599f81ca77f78d247b654bc8e4fa66
Derek Wilson <dcwilson0901@gmail.com>
2023-04-26 14:10:30 -0400

    Messed up some commits

--- template/viewA.js
+++ template/viewA.js
@@ -4,51 +4,76 @@ class ViewA {
         this.con = con;
 
         const div = root.append('div')
-                        .style('position','fixed')
-                        .style('top','0')
-                        .style('left','0')
-                        .style('width','50%')
-                        .style('height','100%')
+            .style('position', 'fixed')
+            .style('top', '0')
+            .style('left', '0')
+            .style('width', '50%')
+            .style('height', '100%')
 
         const svg = div.append('svg')
-                        .style('position','fixed')
-                        .style('top','0')
-                        .style('left','0')
-                        .style('width','50%')
-                        .style('height','100%');
+            .style('position', 'fixed')
+            .style('top', '0')
+            .style('left', '0')
+            .style('width', '50%')
+            .style('height', '100%');
 
-        const rect = svg.append('rect')
-                        .style('y','300')
-                        .style('x','120')
-                        .style('width','250')
-                        .style('height','250')
-                        .style('fill', '#B8860B')
-                        .on('click', ()=>{
-                            this.con.Message(`View A was clicked`);
-                        });
+        // Create a foreignObject element and append it to the SVG
+        var foreignObject = svg.append("foreignObject")
+            .attr("width", 400)
+            .attr("height", 400);
+
+        // Create a div element that will contain the Chart.js chart
+        var chartDiv = foreignObject.append("xhtml:div")
+            .attr("id", "chart-container")
+            .style("width", "100%")
+            .style("height", "100%");
+
+        // Create the heatmap chart
+        var myChart = new Chart(chartDiv, {
+            type: 'heatmap',
+            data: data,
+            options: {
+                title: {
+                    display: true,
+                    text: 'Car Sales by Region'
+                },
+                scales: {
+                    xAxes: [{
+                        ticks: {
+                            fontSize: 10
+                        }
+                    }],
+                    yAxes: [{
+                        ticks: {
+                            fontSize: 10
+                        }
+                    }]
+                }
+            }
+        });
 
         const label = svg.append('text')
-                        .text('View A')
-                        .style('fill', 'white')
-                        .style('font-size', '50')
-                        .attr('y','450')
-                        .attr('x','170')
-                        .on('click', ()=>{
-                            this.con.Message(`View A was clicked`);
-                        });
+            .text('View A')
+            .style('fill', 'white')
+            .style('font-size', '50')
+            .attr('y', '450')
+            .attr('x', '170')
+            .on('click', () => {
+                this.con.Message(`View A was clicked`);
+            });
 
         this.#intraLabel = svg.append('text')
-                        .text(this.name)
-                        .style('fill', 'black')
-                        .style('font-size', '20')
-                        .attr('y','500')
-                        .attr('x','170')
-                        .on('click', ()=>{
-                            this.con.Message(`View A was clicked`);
-                        });
+            .text(this.name)
+            .style('fill', 'black')
+            .style('font-size', '20')
+            .attr('y', '500')
+            .attr('x', '170')
+            .on('click', () => {
+                this.con.Message(`View A was clicked`);
+            });
 
     }
-    Hear(str){
+    Hear(str) {
         this.#intraLabel.text(str);
     }
 }
